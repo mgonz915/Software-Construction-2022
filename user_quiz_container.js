967e3b68dfae3bfcd6dbb2c6af51a192997ac5b0
Michael Gonzalez <47253579+mgonz915@users.noreply.github.com>
2022-11-09 17:14:25 -0700

    Add files via upload

---
+++ user_quiz_container.js
@@ -0,0 +1,57 @@
+var myQuestions = [ 
+    {
+      question: "What is 10/2?",
+      answers: {
+        a: '3',
+        b: '5',
+        c: '115'
+      },
+      correctAnswer: 'b'
+    },
+    {
+      question: "What is 30/3?",
+      answers: {
+        a: '3',
+        b: '5',
+        c: '10'
+      },
+      correctAnswer: 'c'
+    },
+  ];
+
+// Replace Above with a way to get the 3-6 latest quizzes made 
+
+//this class may be a sub function needed in quiz view/contrtoller
+
+
+    var quizContainer = document.getElementById('userQuizSection');
+
+    showQuizContainer(myQuestions,quizContainer);
+
+    function showQuizContainer(questions, quizContainer){
+        // we'll need a place to store the output and the answer choices
+        var output = [];
+        var answers;
+    
+        // for each question...
+        for(var i=0; i<3; i++){
+          
+          
+        
+          // add this question and its answers to the output
+          output.push(
+            '<div class="userQuizContainer">' 
+            + '<a href="./quiz_page.html">' 
+              + '<div style="padding: 20px; margin: 20px; background-color: red;">'+ 'QUIZ_NAME: ' + i + '</div>'
+            + '</a>'
+           + '</div>'
+          );
+
+          
+
+
+        }
+    
+        // finally combine our output list into one string of html and put it on the page
+        quizContainer.innerHTML = output.join('');
+      }
