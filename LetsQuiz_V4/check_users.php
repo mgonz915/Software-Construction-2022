d0787a39fdcd91a4ee0ff9696fa285404190169d
Alexis Navarro <44381479+AlexisNavarro@users.noreply.github.com>
2022-11-10 01:10:50 -0500

    Add files via upload

--- LetsQuiz_V4/check_users.php
+++ LetsQuiz_V4/check_users.php
@@ -1,32 +1,42 @@
-<?php 
+<?php
 session_start();
 require_once "config.php";
-require_once "login.php";
+require_once "user_Log_in.html";
 
-#this is to be able to connect to the database in the local host
-try {
-    $pdo = new PDO($atrr, $username, $password, $opts);
-}catch (PDOException $e){
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
-}
+    try {
+        $pdo = new PDO($atrr, $username, $password, $opts);
+    }catch (PDOException $e){
+        throw new PDOException($e->getMessage(), (int)$e->getCode());
+    }
 
-#To verify the correct fields were submited from the front end
-if(!empty($_POST)){
-    if(isset($_POST['Submit'])){
-        if(isset($_POST['username']) && isset($_POST['password'])){
-            $input_username = $_POST['username'];
-            $input_password = $_POST['password'];
+
+#CREATE A READER METHOD TO READ THE HASHED PASSWORDS
+    if(!empty($_POST)){
+        if(isset($_POST['Submit'])){
+            if(isset($_POST['username']) && isset($_POST['password'])){
+                //verify the user password from the input
+                    $input_username = $_POST['username'];
+                    $input_password = $_POST['password'];
+    
+                    $query = "SELECT * FROM web_user WHERE userName='" . $input_username ."';";
+                    $result = $pdo->query($query);
+                    
+                    //allows to enter log in info if right, if not it will print user not found if log in info is wrong
+                    //if the input is from a user then will search in the user table
+                    if($row = $result->fetch()){
+                        //$_SESSION['user'] = $input_username;
+                        $_SESSION['user'] = $row['firstName']; //will send the first name of the user into the main page
+                        header("Location: main_page.php");
+                    }else{
+                            echo "User not found.";
+                        }
+                      
+                    }//END IF ELSE
+
+             
+            }else{
+                echo "Password is invalid";
             
-            #submitting a query into the database, allowing us to retrieve 
-            $query = "SELECT * FROM users WHERE userName='" . $input_username ."and WHERE password='". $input_password . "';";
-            $result = $pdo->query($query);
+        }//end 3rd if
+    }//end 2nd if
 
-            if($row = $result->fetch()){
-                $_SESSION['user'] = $row['first_name']; //will send the first name of the user into the main page
-                header("Location: main_page.php");
-            }//END IF
-        }//END USERNAME AND PASSWORD POST
-    }//END SUBMIT IF
-}//END POST IF
-
-?>
